4bfa9a9c76869bd10c4a455db4f0fee598ffb0f4
Josh Skrzypczak <josh.skrzypczak@gmail.com>
2018-09-18 23:58:38 -0400

    Pull Retrofit instance creation out of repository

--- app/src/main/java/com/josh/billrssroom/repository/BillRepository.java
+++ app/src/main/java/com/josh/billrssroom/repository/BillRepository.java
@@ -6,22 +6,25 @@ import android.arch.lifecycle.MutableLiveData;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.josh.billrssroom.api.DataService;
+import com.josh.billrssroom.api.RetrofitClient;
 import com.josh.billrssroom.db.BillDatabase;
 import com.josh.billrssroom.model.BillModel;
 import com.josh.billrssroom.model.Rss;
-import com.josh.billrssroom.api.DataService;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
-
+/**
+ * Repository modules handle data operations.
+ * They provide a clean API so that the rest of the app can retrieve this data easily.
+ * They know where to get the data from and what API calls to make when data is updated.
+ * You can consider repositories to be mediators between different data sources, such as persistent
+ * models, web services, and caches.
+ */
 public class BillRepository {
 
     private final DataService apiService;
@@ -39,27 +42,17 @@ public class BillRepository {
             observableBills.postValue(billItems);
         });
 
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-        httpClient.connectTimeout(10, TimeUnit.SECONDS);
-        httpClient.readTimeout(30, TimeUnit.SECONDS);
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(DataService.BASE_URL)
-                .addConverterFactory(SimpleXmlConverterFactory.create())
-                .client(httpClient.build())
-                .build();
-
-        apiService = retrofit.create(DataService.class);
+        apiService = RetrofitClient.getInstance().getRestApi();
     }
 
     public LiveData<List<BillModel>> getFeedItems() {
         final MutableLiveData<List<BillModel>> data = new MutableLiveData<>();
-        apiService.getBillItemsNormally().enqueue(new Callback<Rss>() {
+        apiService.getFeedItems().enqueue(new Callback<Rss>() {
             @Override
             public void onResponse(@NonNull Call<Rss> call, @NonNull Response<Rss> response) {
-                data.setValue(response.body().getChannel().getBillModels());
-
-
+                if (response.isSuccessful()){
+                    data.setValue(response.body().getChannel().getBillModels());
+                }
             }
 
             @Override
