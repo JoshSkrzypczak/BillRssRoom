a9a2aedf05af2cb5dcce6e23e38690c9a1978e43
Josh Skrzypczak <josh.skrzypczak@gmail.com>
2018-09-18 23:59:06 -0400

    Pull Retrofit instance creation out of repository

---
+++ app/src/main/java/com/josh/billrssroom/api/RetrofitClient.java
@@ -0,0 +1,38 @@
+package com.josh.billrssroom.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
+
+public class RetrofitClient {
+
+    private static final String BASE_URL = "http://www.legislature.mi.gov/";
+    private static RetrofitClient INSTANCE;
+    private Retrofit retrofit;
+
+    public static synchronized RetrofitClient getInstance(){
+        if (INSTANCE == null){
+            INSTANCE = new RetrofitClient();
+        }
+        return INSTANCE;
+    }
+
+    public DataService getRestApi(){
+        return retrofit.create(DataService.class);
+    }
+
+    private RetrofitClient() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(loggingInterceptor);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(SimpleXmlConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+    }
+}
