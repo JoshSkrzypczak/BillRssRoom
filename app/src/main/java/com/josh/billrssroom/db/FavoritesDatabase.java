16acae9a6d7a75c075cb8b2524a90c90309fea31
Josh Skrzypczak <josh.skrzypczak@gmail.com>
2018-09-26 00:21:48 -0400

    Adjusting Favorites implementation to work with boolean value

--- app/src/main/java/com/josh/billrssroom/db/FavoritesDatabase.java
+++ app/src/main/java/com/josh/billrssroom/db/FavoritesDatabase.java
@@ -1,18 +1,23 @@
 package com.josh.billrssroom.db;
 
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.Room;
-import android.arch.persistence.room.RoomDatabase;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
 import android.content.Context;
 
-//@Database(entities = {FavoriteEntity.class}, version = 1)
+import com.josh.billrssroom.db.dao.FeedDao;
+import com.josh.billrssroom.model.FeedItem;
+
+@Database(entities = {FeedItem.class}, version = 1)
 public abstract class FavoritesDatabase extends RoomDatabase {
 
     private static FavoritesDatabase INSTANCE = null;
-    private static final String DATABASE_NAME = "favorites";
-//    public abstract FavoriteDao favoriteDao();
+    // TODO: 9/26/2018 Consider using single database across app
+    // See https://stackoverflow.com/questions/48279481/multiple-tables-with-same-type-of-objects-in-room-database
+    private static final String DATABASE_NAME = "favorites.db";
+    public abstract FeedDao favoriteDao();
 
-    public synchronized static FavoritesDatabase getDatabase(Context context){
+    public synchronized static FavoritesDatabase getFavoriteDatabase(Context context){
         if (INSTANCE == null){
             INSTANCE = create(context);
         }
